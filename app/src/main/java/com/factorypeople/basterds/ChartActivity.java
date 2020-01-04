cf584831728ccc849b7aace99111d7302c803e53
ShinJuwon <41102652+onixdragon1@users.noreply.github.com>
2020-01-04 21:38:31 +0900

    ready to use match&chart xml_2
    Just 'ready' step

--- app/src/main/java/com/factorypeople/basterds/ChartActivity.java
+++ app/src/main/java/com/factorypeople/basterds/ChartActivity.java
@@ -1,7 +1,20 @@
 package com.factorypeople.basterds;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChartActivity extends AppCompatActivity {
+    String pId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+
+        Intent intent = getIntent();
+        pId = intent.getStringExtra("id");
+    }
 
 }
--- app/src/main/java/com/factorypeople/basterds/MatchActivity.java
+++ app/src/main/java/com/factorypeople/basterds/MatchActivity.java
@@ -1,7 +1,20 @@
 package com.factorypeople.basterds;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MatchActivity extends AppCompatActivity {
+    String pId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+
+        Intent intent = getIntent();
+        pId = intent.getStringExtra("id");
+    }
 
 }
